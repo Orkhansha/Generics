1f972c16527885814a7ce3e6ea85afdc0aeae9a9
Orkhansha <orkhansha@code.edu.az>
2022-07-07 23:40:15 +0400

    Added Generics cs files

---
+++ Generics/Generics/Models/Kelvin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics.Models
+{
+    public class Kelvin
+    {
+        public double Degree { get; set; }
+
+        public Kelvin(double degree)
+        {
+            Degree = degree;
+        }
+        
+        //Bu formadada yaza bilerik eyni weydi))
+        
+        //public static implicit operator Celsius(Kelvin kelvin)
+        //{
+        //    return new Celsius(kelvin.Degree + 273.15);
+        //}
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿using Generics.Models;
+using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius celsius = new Celsius(10);
+
+            Kelvin kelvin = celsius;
+
+            Console.WriteLine(kelvin.Degree);
+        }
+    }
+
+}
